d828eb63fecab48f04933e53480f0412ce5eafc4
Luke Cannon <lukewzcannon@gmail.com>
2023-04-02 00:05:44 +0800

    Review page populated

--- src/components/review/ReviewPage.js
+++ src/components/review/ReviewPage.js
@@ -1,5 +1,80 @@
+import useFormContext from "../../hooks/use-form-context";
+import ContentPane from "../common/ContentPane";
+
+const border = (
+  <div className="w-full h-[2px] m-1 bg-gradient-to-r from-transparent via-gray-400 to-transparent"></div>
+);
+
+function ReviewItem({ label, children }) {
+  return (
+    <div className="flex w-full justify-between">
+      <div className="italic text-left">{label}</div>
+      <div className="text-right">{children}</div>
+    </div>
+  );
+}
+
 function ReviewPage() {
-  return <div>Review</div>;
+  const {
+    gamesPlayed,
+    realm,
+    skills,
+    investments,
+    spells,
+    crafts,
+    potions,
+    ceremonies,
+    heroName,
+    archetype,
+    warband,
+    sect,
+  } = useFormContext();
+
+  return (
+    <div className="flex justify-center">
+      <ContentPane background={realm ? realm.image : null}>
+        <div className="flex flex-col items-center mt-2">
+          <div className="text-xl leading-6">{heroName}</div>
+          <div className="italic opacity-70">
+            {realm.name}
+            {archetype ? " " + archetype : ""}
+          </div>
+          {border}
+          <ReviewItem label="Summits attended">{gamesPlayed}</ReviewItem>
+          <ReviewItem label="Investment">
+            {investments.map((i) => i.name).toString()}
+          </ReviewItem>
+          {(warband || sect) && border}
+          {warband && <ReviewItem label="Warband">{warband}</ReviewItem>}
+          {sect && <ReviewItem label="Sect">{sect}</ReviewItem>}
+          {border}
+          <ReviewItem label="Skills">
+            {skills.map((s) => " " + s.name)}
+          </ReviewItem>
+          {spells.length > 0 && (
+            <ReviewItem label="Spells">
+              {spells.map((s) => " " + s.name).toString()}
+            </ReviewItem>
+          )}
+          {crafts.length > 0 && (
+            <ReviewItem label="Crafts">
+              {crafts.map((s) => " " + s.name).toString()}
+            </ReviewItem>
+          )}
+          {potions.length > 0 && (
+            <ReviewItem label="Potions">
+              {potions.map((s) => " " + s.name).toString()}
+            </ReviewItem>
+          )}
+          {ceremonies.length > 0 && (
+            <ReviewItem label="Ceremonies">
+              {ceremonies.map((s) => " " + s.name).toString()}
+            </ReviewItem>
+          )}
+        </div>
+      </ContentPane>
+    </div>
+  );
 }
 
 export default ReviewPage;
