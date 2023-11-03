f5ee15bc72bc26d3107deb1b2e05ee133bb3f5c7
Luke <lukewzcannon@gmail.com>
2023-11-03 22:25:36 +0800

    Put back blur, it didn't help mobile

--- src/components/common/Login/Login.js
+++ src/components/common/Login/Login.js
@@ -237,6 +237,7 @@ function Login({ show, handleClose, user }) {
             onClose={handleClose}
             aria-labelledby='modal-modal-title'
             aria-describedby='modal-modal-description'
+            style={{ backdropFilter: "blur(2px)" }}
         >
             <ModalBox>
                 {user ? (
