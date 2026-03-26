3e83000f441c9ccf5fd3382bf75cc65bad2890de
Luke <lukewzcannon@gmail.com>
2026-03-26 18:31:58 +0800

    Make cloudflare happy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+/*
+Next step of the upgrade to allow for API routes:
+  1. npm install @cloudflare/next-on-pages
+  2. Change the build command from next build to npx @cloudflare/next-on-pages
+  3. Remove output: 'export' from next.config.ts
+  4. Add a wrangler.toml for Cloudflare Workers config
+*/
+
 const nextConfig: NextConfig = {
 	/* config options here */
+	output: "export",
 	distDir: "build",
 	reactStrictMode: true,
 	experimental: {
